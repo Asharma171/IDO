aaf4cbea65f30cdba4bee9c3dafe16ad31bd9020
Ankit Sharma <61593608+Asharma171@users.noreply.github.com>
2022-02-01 13:33:43 +0530

    Delete contracts directory

--- contracts/AS.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.1;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract AS is ERC20 {
-  constructor() ERC20("AS", "AS") {
-    _mint(msg.sender, 1000000e18); //1000000 tokens totalsupply
-  }
-}
--- contracts/AST.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.1;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract AST is ERC20 {
-  constructor() ERC20("AST", "ASt") {
-    _mint(msg.sender, 1000000000e18); //1000 tokens totalsupply
-  }
-}
